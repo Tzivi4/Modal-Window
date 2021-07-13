96ceb58ded54115e591f5e8738cef7ea716efa9e
Tzivi <gelsteintzivisantiago@gmail.com>
2021-07-13 14:42:59 -0300

    Add better experience in modal and add sans serif font family

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const openButton = document.getElementById('openButton')
 const closeButton = document.getElementById('closeButton')
+const modalContainer = document.getElementById('modalContainer')
 const modal = document.getElementById('modal')
 
+modal.addEventListener('click', e => {
+  e.stopPropagation()
+})
+
+modalContainer.addEventListener('click', () => {
+  modalContainer.style.display = 'none'
+})
+
 openButton.addEventListener('click', () => {
-    
-  modal.style.display = 'initial'
+  modalContainer.style.display = 'grid'
 })
 
 closeButton.addEventListener('click', () => {
-
-  modal.style.display = 'none'
+  modalContainer.style.display = 'none'
 })
